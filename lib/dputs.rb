420cff1950361c5bd2141d9bd56c9c5fff8f2860
RAHUL DHOLE <rdhole95@gmail.com>
2025-06-06 00:35:43 +0200

    init dputs

---
+++ lib/dputs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dputs/version"
+
+module Dputs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/dputs_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dputs do
+  it "has a version number" do
+    expect(Dputs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
